59d759de7428fd905928d2ad16738cf479c439da
Rylan Wasson <wassonr@wwu.edu>
2024-09-21 12:36:43 -0700

    changed folder structure to add frontend

---
+++ backend/MTGPrices-Server.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require('express')
+const app = express()
+const port = 3000
+const axios = require('axios').default
+const mongoose = require('mongoose')
+const fs = require('fs')
+const {Card, Price } = require('./models/CardModels')
+
+const{updateAllCards, uploadNewCards,uploadAllCards, uploadPrices, deletePrices} = require('./db/CardOperations')
+const{fetchBulkCardData} = require('./services/DataService')
+const {readData} = require('./services/DataService')
+const {initCronJobs} = require('./services/Scheduler')
+
+// connect to db
+mongoose.connect(process.env.DATABASE_URL)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log('Mongo DB connection error: ', err))
+
+
+//fetchBulkCardData()
+// const cards = readData('./bulkdata/output.json')
+//uploadAllCards(cards)
+// uploadPrices(cards)
+//deletePrices(1)
+fetchBulkCardData()
+initCronJobs()
+
+app.get('/', (req, res) => {
+    res.send('Hello there')
+})
+
+app.listen(port, () => {
+    console.log(`MTGPrices listening on port ${port}`)
+})
---
+++ backend/services/Scheduler.js
@@ -0,0 +1,76 @@
+const cron = require('node-cron')
+const { readData, fetchBulkCardData } = require('./DataService')
+const {     
+    updateAllCards, 
+    uploadNewCards,
+    uploadPrices,
+    deletePrices
+} = require('../db/CardOperations')
+const filePath = './bulkdata/output.json'
+
+/*
+ # ┌────────────── second (optional)
+ # │ ┌──────────── minute
+ # │ │ ┌────────── hour
+ # │ │ │ ┌──────── day of month
+ # │ │ │ │ ┌────── month
+ # │ │ │ │ │ ┌──── day of week
+ # │ │ │ │ │ │
+ # │ │ │ │ │ │
+ # * * * * * *
+*/
+
+async function initCronJobs() {
+    /*
+    Once every day
+    Update price information and delete stale data
+    */
+    cron.schedule('0 0 0 * * *', async () => {
+        try {
+            await fetchBulkCardData()
+            
+            const data = readData(filePath)
+            await uploadPrices(data)
+
+            let thresholdDate = new Date(Date.now())
+            thresholdDate.setDate(thresholdDate.getDate() - 31) // Magic number
+            await deletePrices(thresholdDate)
+        } catch (err) {
+            console.error('Error in daily cron job:', err)
+        }
+    })
+
+    /*
+    Once every 7 days
+    Upload any fresh cards from the previous week
+    */
+    cron.schedule('0 0 0 */7 * *', async () => {
+        try {
+            const data = readData(filePath)
+            let thresholdDate = new Date(Date.now())
+            thresholdDate.setDate(thresholdDate.getDate() - 7) // Magic Number
+            
+            await uploadNewCards(data, thresholdDate)
+        } catch (err) {
+            console.error('Error in weekly cron job:', err)
+        }
+    })
+
+    /*
+    Once a month
+    Update any oracle changes to cards
+    */
+    cron.schedule('0 0 0 1 * *', async () => {
+        try {
+            const data = readData(filePath)
+
+            await updateAllCards(data)
+        } catch (err) {
+            console.error('Error in monthly cron job:', err)
+        }
+    })
+}
+
+module.exports = {
+    initCronJobs
+}
